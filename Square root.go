071719d98793ea1da93f89cc04765c2c2f4e10f9
AntonKhPI <152258209+AntonKhPI@users.noreply.github.com>
2024-01-07 17:31:48 +0200

    Update Square root.go

--- Square root.go
+++ Square root.go
@@ -1,10 +1,24 @@
-package first
+package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math"
 )
 
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for {
+		if z != math.Sqrt(x) {
+			z -= (z*z - x) / (2 * z)
+		} else {
+			return z
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Моє улюблене число:", rand.Intn(10))
+	var i float64
+	fmt.Print("Write a number: ")
+	fmt.Scan(&i)
+	fmt.Println("Square root of your number is ", Sqrt(i))
 }
